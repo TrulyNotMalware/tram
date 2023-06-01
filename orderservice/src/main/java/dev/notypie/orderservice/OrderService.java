41254074d4e6ac7a48ea2c7ed99ead143d9f6bd7
TrulyNotMalware <notleebutyee@gmail.com>
2023-06-01 18:33:50 +0900

    Init : init templates

---
+++ orderservice/src/main/java/dev/notypie/orderservice/OrderService.java
@@ -0,0 +1,11 @@
+package dev.notypie.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderService {
+    public static void main(String[] args){
+        SpringApplication.run(OrderService.class);
+    }
+}
